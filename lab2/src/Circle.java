d6ef1158ba131dc93990ade1caafa10f7f044996
deep <egor.grachev.mail@gmail.com>
2021-09-03 19:19:43 +0300

    added length, square - Lab2

--- lab2/src/Circle.java
+++ lab2/src/Circle.java
@@ -1,11 +1,24 @@
+//import jdk.nashorn.internal.objects.Global;
+import javax.swing.JOptionPane;
+
 public class Circle {
     private float radius;
     private float diameter;
+    private float length;
+    private float square;
     private String name = "Окружность";
 
     public Circle(float r, float d) {
         radius = r;
         diameter = d;
+    }
+
+    public void calcLength() {
+        length = (float) (Math.PI * diameter);
+    }
+
+    public void calcSquare() {
+        square = (float) (Math.PI * Math.pow(2, radius));
     }
 
     public float getRadius() {
@@ -18,6 +31,16 @@ public class Circle {
 
     public String getName() {
         return name;
+    }
+
+    public float getLength() {
+        calcLength();
+        return length;
+    }
+
+    public float getSquare() {
+        calcSquare();
+        return square;
     }
 
     public void setRadius(float r) {
--- lab2/src/CircleTest.java
+++ lab2/src/CircleTest.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 
 public class CircleTest {
+    public static void showInfo(Circle circ1) {
+        System.out.print("Радиус: " + circ1.getRadius() + "; Диаметр: " + circ1.getDiameter() + "; Длина: " + circ1.getLength() + "; Площадь: " + circ1.getSquare() + ";");
+    }
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Изучение Java.\nЛабораторная работа 2. Вариант 1.\n");
 
         Circle circ1 = new Circle(5, 15);
+        
 
         System.out.println("Имя: " + circ1.getName());
-        System.out.print("Радиус: " + circ1.getRadius() + "; Диаметр: " + circ1.getDiameter() + ";");
+        showInfo(circ1);
 
         System.out.println("\n\nИзменение значений");
         System.out.print("Радиус: ");
@@ -17,6 +21,6 @@ public class CircleTest {
         System.out.print("Диаметр: ");
         circ1.setDiameter(scanner.nextFloat());
 
-        System.out.println("\nРадиус: " + circ1.getRadius() + "; Диаметр: " + circ1.getDiameter());
+        showInfo(circ1);
     }
 }
