7b74ea5397596a6f96f04cbca35c7b4d86767c61
deep <egor.grachev.mail@gmail.com>
2021-09-09 17:15:10 +0300

    changed file structure

---
+++ structures_algorithms/practice1/src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App {
+    public static int calc(int inputNumber) {
+        int outNum = 1;
+
+        for(int i=1;i<=inputNumber;i++) {
+            outNum = outNum * i;
+        }
+
+        return outNum;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = 1;
+
+        System.out.println("Изучение Java.\nТест\n");
+
+        inputNumber = scanner.nextInt();
+        
+        System.out.println("Ответ: ");
+        System.out.println(calc(inputNumber));
+    }
+}
